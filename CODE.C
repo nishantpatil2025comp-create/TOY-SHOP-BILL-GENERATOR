c5ee4c4b81093bc11169121b8ef9afa8c7234209
nishantpatil2025comp-create <nishantpatil2025.comp@mmcoe.edu.in>
2025-10-06 16:25:38 +0530

    Create CODE.C

---
+++ CODE.C
@@ -0,0 +1,18 @@
+int main() {
+    
+  int total;
+    char name, customerid;
+    printf(" ROY TOY SUPER SHOP\n");
+    printf("====================\n\n\n");
+    printf("at ASHOKA PALACE AMRAVATI , MAHARASHTRA\n\n\n");
+    printf(" wellcome to roy toy shop\n\n");
+    printf("enter your name:\n\n ");
+    scanf("%s", &name);
+    printf("enter your customer id:\n\n");
+    scanf("%s",&customerid);
+    printf("YOUR LIST OF PRODUCTS \n 1) barbie doll set  -999 rupees\n 2) RC car - 799 rupees \n 3) Cricket bat - 1299rupees\n 4) football-988\n 5)wooden horse-399\n\n\n\n");
+total= 999+799+1299+988+399;
+printf("total price: %d\n\n", total );
+ printf("THANKS FOR SHOPPING ");
+    return 0;
+}
